fe65c4a989f5d14292cf34a8d2bdb8d219431a1c
paul domino <p.domino@protomail.com>
2020-07-18 16:21:46 -0400

    Use SNS to notify when portfolio is deployed

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -5,22 +5,65 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_bucket = s3.Bucket('portfolio.bluegreywall.com')
-    build_bucket = s3.Bucket('portfoliobuild.bluegreywall.com')
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:437570009687:deployPortfolioTopic')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.bluegreywall.com')
+        build_bucket = s3.Bucket('portfoliobuild.bluegreywall.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    return 'Hello from the little blue marble'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                #print nm
+
+
+        print "Job Completed!"
+        topic.publish(Message="Deployed sucessfully! (lambda python boto3 sns)", Subject="Portfolio Deployed")
+    except:
+        topic.publish(Message="Deployment failed! (lambda python boto3 sns)", Subject="Portfolio Deployed")
+        raise
+
+
+    return 'from little blue marble'
+
+
+# ----------------------------------------------------------------- #
+# below is the code that we used prior to making changes
+# in aws lambda editor, which is above
+# ----------------------------------------------------------------- #
+
+# import boto3
+# import StringIO
+# import zipfile
+# import mimetypes
+#
+# def lambda_handler(event, context):
+#
+#     s3 = boto3.resource('s3')
+#
+#     portfolio_bucket = s3.Bucket('portfolio.bluegreywall.com')
+#     build_bucket = s3.Bucket('portfoliobuild.bluegreywall.com')
+#
+#     portfolio_zip = StringIO.StringIO()
+#     build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+#
+#     with zipfile.ZipFile(portfolio_zip) as myzip:
+#         for nm in myzip.namelist():
+#             obj = myzip.open(nm)
+#             portfolio_bucket.upload_fileobj(obj, nm,
+#             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+#             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+#
+#     return 'Hello from the little blue marble'
 
 # ----------------------------------------------------------------- #
 # below is the code that we used prior to making changes
